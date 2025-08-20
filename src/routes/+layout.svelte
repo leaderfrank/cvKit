fb0a41d0711ad6503670a8134b8763de1d0b9626
Mohammed Sultan <werrew230@gmail.com>
2025-08-20 03:52:27 +0300

    Add PWA assets including icons and manifest for cvKit application
    - Added favicon.png for the application.
    - Created manifest.json with app details and icon references.
    - Included various icon sizes: pwa-64x64.png, pwa-192x192.png, pwa-512x512.png, and maskable-icon-512x512.png for better compatibility across devices.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import { ModeWatcher } from "mode-watcher";
+	import { ModeWatcher } from 'mode-watcher';
+	import { onMount } from 'svelte';
 	let { children } = $props();
+	// Detect service worker updates
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener('updatefound', () => {
+			const newSW = registration.installing;
+			newSW?.addEventListener('statechange', () => {
+				if (newSW.state == 'installed') {
+					if (confirm('A new version is available. Do you want to refresh?')) {
+						newSW.postMessage({ type: 'SKIP_WAITING' });
+						window.location.reload();
+					}
+				}
+			});
+		});
+	}
+
+	onMount(() => {
+		detectSWUpdate();
+	});
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
- 
 <ModeWatcher />
 {@render children?.()}
